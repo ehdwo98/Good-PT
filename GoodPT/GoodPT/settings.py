04ca3086d5814c29a4bdb187a483a7db4a758020
ysh0214727 <tjdghks0214@gmail.com>
2023-12-18 15:31:56 +0900

    mydb connect

--- GoodPT/GoodPT/settings.py
+++ GoodPT/GoodPT/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -74,9 +75,13 @@ WSGI_APPLICATION = "GoodPT.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default':{
+        'ENGINE' : 'django.db.backends.mysql',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
+        'NAME' : 'mydb',
+        'USER' : 'root',
+        'PASSWORD' : 'aivle',
     }
 }
 
