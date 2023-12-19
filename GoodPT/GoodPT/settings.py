bba923eeb385abb414400699c535156d10fda930
ysh0214727 <tjdghks0214@gmail.com>
2023-12-19 17:43:44 +0900

    login 디자인 긁어오기

--- GoodPT/GoodPT/settings.py
+++ GoodPT/GoodPT/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'GoodPT','static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
