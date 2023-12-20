b86620882d1b43ed4bbf205bd283f1fc72ab25bb
Dong-Jae <wlsdk1095sd@gmail.com>
2023-12-20 16:06:31 +0900

    fix
    dsaf

--- GoodPT/board/views.py
+++ GoodPT/board/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 
-from .models import Question, 
+from .models import Question, Answer
 
 
 def index(request):
