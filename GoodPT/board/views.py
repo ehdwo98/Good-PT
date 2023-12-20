473f83e1e8a5b9207681662982e1ef8a80dd4ac6
Dong-Jae <wlsdk1095sd@gmail.com>
2023-12-20 16:02:16 +0900

    fix

--- GoodPT/board/views.py
+++ GoodPT/board/views.py
@@ -7,17 +7,17 @@ from .models import Question, Answer
 def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list': question_list}
-    return render(request, 'pybo/question_list.html', context)
+    return render(request, 'board/question_list.html', context)
 
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
-    return render(request, 'pybo/question_detail.html', context)
+    return render(request, 'board/question_detail.html', context)
 
 
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
     answer.save()
-    return redirect('pybo:detail', question_id=question.id)
+    return redirect('board:detail', question_id=question.id)
