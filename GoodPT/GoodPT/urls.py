7ef47b3c89a669c315e765b2443315cee2805a82
Suhyun Rim <ish000118@gmail.com>
2023-12-21 09:42:42 +0900

    add main page

--- GoodPT/GoodPT/urls.py
+++ GoodPT/GoodPT/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.shortcuts import render
+
+def index(request):
+    return render(request,'index.html')
 
 urlpatterns = [
+    path("", index),
     path("admin/", admin.site.urls),
     path("login/",include('accounts.urls')),
     path("board/",include('board.urls'))
