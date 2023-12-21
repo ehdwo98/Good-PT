0d24883619b787cab22f2065aad884bb2018a96f
Dong-Jae <wlsdk1095sd@gmail.com>
2023-12-21 16:40:14 +0900

    게시판 nav바, 고도화

--- GoodPT/accounts/urls.py
+++ GoodPT/accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'login'
+
 urlpatterns = [
     path('',views.test1),
     path('naver-login',views.test2),
-    path('google-login',views.test3)
-
+    path('google-login',views.test3),
 ]
