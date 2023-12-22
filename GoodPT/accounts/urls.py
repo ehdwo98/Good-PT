457ec7784450a9fffbd7235b063d5a0f394e0f0b
shinshdd <ssangi0414@gmail.com>
2023-12-22 09:55:29 +0900

    bugfix : 상희님 충돌해결2

--- GoodPT/accounts/urls.py
+++ GoodPT/accounts/urls.py
@@ -6,10 +6,6 @@ app_name = 'login'
 urlpatterns = [
     path('',views.test1),
     path('naver-login',views.test2),
-<<<<<<< HEAD
-    path('google-login',views.test3),
-=======
     path('naverCallback',views.test3)
 
->>>>>>> 25d4f1d38e7e3743eafa15a1f549e5fbf2f282eb
 ]
