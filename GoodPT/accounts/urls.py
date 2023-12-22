9bb9829735f8862ab4794f14c607383451c9ef80
Dong-Jae <wlsdk1095sd@gmail.com>
2023-12-22 18:30:20 +0900

    fix

--- GoodPT/accounts/urls.py
+++ GoodPT/accounts/urls.py
@@ -6,19 +6,7 @@ app_name = 'login'
 urlpatterns = [
     path('',views.test1),
     path('naver-login',views.test2),
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     path('naverCallback',views.test3),
     path('privacy',views.test4)
-=======
-=======
->>>>>>> 9d31fda (임시 저장)
-    path('naverCallback',views.test3)
->>>>>>> 9d31fda (임시 저장)
-=======
-    path('naverCallback',views.test3),
-    path('privacy',views.test4)
->>>>>>> 6a832bf (pricing table 구현)
 
 ]
