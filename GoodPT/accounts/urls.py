f4406bb957e96f8eaae3c9708cc1946e69b5314f
Dong-Jae <wlsdk1095sd@gmail.com>
2023-12-22 17:27:15 +0900

    임시 저장

--- GoodPT/accounts/urls.py
+++ GoodPT/accounts/urls.py
@@ -6,7 +6,11 @@ app_name = 'login'
 urlpatterns = [
     path('',views.test1),
     path('naver-login',views.test2),
+<<<<<<< HEAD
     path('naverCallback',views.test3),
     path('privacy',views.test4)
+=======
+    path('naverCallback',views.test3)
+>>>>>>> 9d31fda (임시 저장)
 
 ]
