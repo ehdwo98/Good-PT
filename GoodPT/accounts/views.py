b2de4ad8c111ef86fc01964d346acabaa1a69fdb
ysh0214727 <tjdghks0214@gmail.com>
2023-12-24 14:34:30 +0900

    login not sociallogin work

--- GoodPT/accounts/views.py
+++ GoodPT/accounts/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-# Create your views here.
-
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 def test1(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            auth_login(request,form.get_user())
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    context = {'form' : form}
+    return render(request,'login.html',context)
 
 def test2(request):
     return render(request,'naver.html')
