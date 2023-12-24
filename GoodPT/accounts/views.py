2fb10d3dc96a7f86a2d288cecda81e8fb81e7e0b
ysh0214727 <tjdghks0214@gmail.com>
2023-12-24 16:19:00 +0900

    register 구현

--- GoodPT/accounts/views.py
+++ GoodPT/accounts/views.py
@@ -1,19 +1,29 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth import views as auth_views
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
+
 def test1(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request,request.POST)
-        print(request.POST)
-        if form.is_valid():
-            auth_login(request,form.get_user())
-            return redirect('/')
-    else:
-        form = AuthenticationForm()
-    context = {'form' : form}
-    return render(request,'login.html',context)
+        if 'login' in request.POST:
+            form = AuthenticationForm(request,request.POST)
+            if form.is_valid():
+                auth_login(request,form.get_user())
+                return redirect('/')
+        elif 'register' in request.POST:
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                print('register success')
+                return redirect('/login')
+            else:
+                print('register failed')
+    # else:
+    #     form = AuthenticationForm()
+        
+    # context = {'form' : form}
+    return render(request,'login.html')
 
 def test2(request):
     return render(request,'naver.html')
