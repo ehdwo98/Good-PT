c19476393a595919a8c498c9d94debcd0b894482
Dong-Jae <wlsdk1095sd@gmail.com>
2023-12-26 17:33:27 +0900

    feat: 게시판 검색기능

--- GoodPT/board/views/base_views.py
+++ GoodPT/board/views/base_views.py
@@ -2,13 +2,24 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
+from django.db.models import Q
+
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
     question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
     paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'question_list': page_obj}
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
     return render(request, 'board/question_list.html', context)
 
 
