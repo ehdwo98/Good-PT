4bc52485455055b46560efc3ae554a2d79b581ba
ysh0214727 <tjdghks0214@gmail.com>
2024-01-02 11:03:45 +0900

    erase login file and setting report app

--- GoodPT/report/views.py
+++ GoodPT/report/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login as auth_login
+
+def report(request):
+    return render(request,'report.html')
