2692a67e67ecbed8a7c856bd7cd79021df939b8f
Dong-Jae <wlsdk1095sd@gmail.com>
2024-01-03 16:00:24 +0900

    fix: 장고 datetime 한국 시간대로 설정

--- GoodPT/GoodPT/settings.py
+++ GoodPT/GoodPT/settings.py
@@ -136,11 +136,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = 'Asia/Seoul'  # 한국 시간 적용 
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
+
+USE_TZ = False  # False 로 설정해야 DB에 변경 된 TIME_ZONE 이 반영 됨 
 
 
 # Static files (CSS, JavaScript, Images)
