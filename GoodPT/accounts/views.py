5445f4427d84450da79ab232b2db69bac374044b
ysh0214727 <tjdghks0214@gmail.com>
2024-01-03 11:21:08 +0900

    register error message

--- GoodPT/accounts/views.py
+++ GoodPT/accounts/views.py
@@ -4,7 +4,8 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login as auth_login
 from django.contrib import messages
-
+import re
+from accounts.const import errorDictionary
 def test1(request):
     if request.method == 'POST':
         if 'login' in request.POST:
@@ -25,6 +26,11 @@ def test1(request):
                 return redirect('/login')
             else:
                 print('register failed')
+                errors = []
+                for field, error_list in form.errors.items():
+                    match = re.search(r'<li>(.*?)</li>', str(error_list))
+                    errors.append(match.group(1))
+                messages.warning(request,errorDictionary[errors[0]])
     
     return render(request,'login.html')
 
