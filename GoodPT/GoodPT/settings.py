b85bf2869a599b5e9e7ee01548223f32e5a0c9f1
Dong-Jae <wlsdk1095sd@gmail.com>
2024-01-04 11:47:22 +0900

    fix: css 미적용 settings

--- GoodPT/GoodPT/settings.py
+++ GoodPT/GoodPT/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-hug5u5j)8%_npq+49fgx-&-9p!n78j-$c08x!np6rortc%u*mr"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
 
 
 # Application definition
