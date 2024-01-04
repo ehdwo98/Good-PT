4ed7a94845fbf5356ab9253c6a0071060208646a
Dong-Jae <wlsdk1095sd@gmail.com>
2024-01-04 09:42:59 +0900

    fix: board 한국 시간대로 변경

--- GoodPT/board/models.py
+++ GoodPT/board/models.py
@@ -19,8 +19,8 @@ class Question(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE, related_name='author_question')
     subject = models.CharField(max_length=200)
     content = models.TextField()
-    create_date = models.DateTimeField()
-    modify_date = models.DateTimeField(null=True, blank=True)
+    create_date = models.DateTimeField(auto_now_add = True)
+    modify_date = models.DateTimeField(auto_now = True, null=True, blank=True)
     voter = models.ManyToManyField(User, related_name='voter_question')
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category_question')
     
@@ -46,8 +46,8 @@ class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
-    create_date = models.DateTimeField()
-    modify_date = models.DateTimeField(null=True, blank=True)
+    create_date = models.DateTimeField(auto_now_add = True)
+    modify_date = models.DateTimeField(auto_now = True, null=True, blank=True)
     voter = models.ManyToManyField(User, related_name='voter_answer')  # 추천인 추가
 
     def __str__(self):
@@ -84,8 +84,8 @@ class Answer(models.Model):
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comment')
     content = models.TextField()
-    create_date = models.DateTimeField()
-    modify_date = models.DateTimeField(null=True, blank=True)
+    create_date = models.DateTimeField(auto_now_add = True)
+    modify_date = models.DateTimeField(auto_now = True, null=True, blank=True)
     question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
 
