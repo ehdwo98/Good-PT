a863e06333412c718e32d631c25e9790d8d69f7b
Dong-Jae <wlsdk1095sd@gmail.com>
2024-01-08 14:42:16 +0900

    fix: mypage profile models

--- GoodPT/mypage/models.py
+++ GoodPT/mypage/models.py
@@ -1,15 +1,19 @@
 from django.db import models
-from django.conf import settings
 # Create your models here.
 from report.models import REPORT
+from django.contrib.auth.models import User
+import os
+from uuid import uuid4
+
+def get_file_path(instance, filename):
+    user_id = instance.user_id
+    uuid_name = uuid4().hex
+    ext = os.path.splitext(filename)[-1]
+    return f'common/profile/{user_id}/{uuid_name}{ext}'
 
 class Profile(models.Model):
-    user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,verbose_name='사용자')
-    intro=models.TextField()
-    useremail=models.EmailField(max_length=128,verbose_name='사용자_이메일')
-    phone_number = models.CharField(max_length=20,verbose_name='사용자_전화번호')
-    address = models.CharField(max_length=50, verbose_name='사용자_주소')
-    
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=get_file_path, null=True, blank=True)
     report=models.ForeignKey(REPORT, on_delete=models.CASCADE, related_name='user_report')
     
     def __str__(self):
