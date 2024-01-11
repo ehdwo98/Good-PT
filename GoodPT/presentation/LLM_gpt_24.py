0c1fee28a3854093c119af996a1f246a5d59703b
finale22 <86143422+finale22@users.noreply.github.com>
2024-01-11 16:23:10 +0900

    최종본

--- GoodPT/presentation/LLM_gpt_24.py
+++ GoodPT/presentation/LLM_gpt_24.py
@@ -25,7 +25,8 @@ import numpy as np
 
 def question_contents(content):
     # API 연동
-    OPENAI_API_KEY = "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"
+    # OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = "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"
     client = OpenAI(api_key=OPENAI_API_KEY)
 
     # GPT-3.5-TURBO API
@@ -61,7 +62,7 @@ def question_contents(content):
 # 발표 내용 분석
 def content_analysis(content):
     # API 연동
-    OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = "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"
     client = OpenAI(api_key=OPENAI_API_KEY)
 
     # GPT-3.5-TURBO API
@@ -70,7 +71,7 @@ def content_analysis(content):
         messages = [
             {
                 "role" : "system",
-                "content" : "내 발표 내용을 분석하고 보완할 점을 평가한 리포트를 한 문단으로 얘기해줘."
+                "content" : "내 발표 내용을 분석하고 보완할 점을 평가한 리포트를 한 문단으로 얘기해줘. \"발표 내용 분석 결과는 다음과 같습니다.\"라고 시작해."
             },
             {
                 "role" : "user",
@@ -89,12 +90,12 @@ def content_analysis(content):
 # gesture: gesture_analysis.py의 gesture_analysis()의 반환 값인 gesture (제스처 비율)
 def gesture_analysis_return_rate(gesture):
     # API 연동
-    OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = "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"
     client = OpenAI(api_key=OPENAI_API_KEY)
     
     # 제스처 평가 멘트
     gesture_percentage = gesture * 100
-    content = f"제스처 비율은 (제스처 동작 시간 / 전체 발표 시간)이야. 제스처 시간 평가 기준은 값이 10%보다 낮으면 많이 정적인 거고 21%보다 높으면 제스처가 너무 많은 거야. 값이 10% 초과 21% 미만이면 적절한 제스처를 하고 있다는 거야. 제스처 비율 값이 10% 이하면 제스처를 사용하라는 식으로 21% 이상이면 제스처를 좀 줄이라는 식으로 평가해야돼. 내 제스처 비율은 {np.round(gesture_percentage)}%이야. 이 제스처 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘."
+    content = f"제스처 비율은 (제스처 동작 시간 / 전체 발표 시간)이야. 제스처 시간 평가 기준은 값이 10%보다 낮으면 많이 정적인 거고 21%보다 높으면 제스처가 너무 많은 거야. 값이 10% 초과 21% 미만이면 적절한 제스처를 하고 있다는 거야. 제스처 비율 값이 10% 이하면 제스처를 사용하라는 식으로 21% 이상이면 제스처를 좀 줄이라는 식으로 평가해야돼. 내 제스처 비율은 {np.round(gesture_percentage)}%이야. 이 제스처 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘. \"손 제스처 분석 결과는 다음과 같습니다.\"라고 시작해."
 
     # GPT-3.5-TURBO API
     completion = client.chat.completions.create(
@@ -121,17 +122,17 @@ def gesture_analysis_return_rate(gesture):
 # gaze: gesture_analysis.py의 gesture_analysis()의 반환 값인 gaze (정면 응시 비율)
 def gaze_analysis(gaze):
     # API 연동
-    OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = "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"
     client = OpenAI(api_key=OPENAI_API_KEY)
     
     gaze_percentage = gaze * 100
     
     if gaze > 0:
         # 정면 응시 평가 멘트
-        content = f"발표의 정면 응시 비율은 (정면 응시 프레임의 합 / 전체 프레임의 합)%야. 발표의 정면 응시 비율은 발표 중 정면을 응시하며 발표한 비율을 뜻 해. 발표의 정면 응시 비율이 60% 이상 90% 이하면 발표를 잘하고 있다는 것이고 60%보다 낮으면 정면을 적게 인식하여 청중의 주의가 산만하고 90%보다 높으면 너무 정면만 응시하여 청중의 집중을 이끌지 못해. 내 발표의 정면 응시 비율은 {np.round(gaze_percentage, 2)}%야. 이 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘."
+        content = f"발표의 정면 응시 비율은 (정면 응시 프레임의 합 / 전체 프레임의 합)%야. 발표의 정면 응시 비율은 발표 중 정면을 응시하며 발표한 비율을 뜻 해. 발표의 정면 응시 비율이 60% 이상 90% 이하면 발표를 잘하고 있다는 것이고 60%보다 낮으면 정면을 적게 인식하여 청중의 주의가 산만하고 90%보다 높으면 너무 정면만 응시하여 청중의 집중을 이끌지 못해. 내 발표의 정면 응시 비율은 {np.round(gaze_percentage, 2)}%야. 이 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘. \"정면 응시 분석 결과는 다음과 같습니다.\"라고 시작해."
     else:
         gaze_percentage *= -1
-        content = f"발표의 정면 응시 비율은 (정면 응시 프레임의 합 / 전체 프레임의 합)%야. 발표의 정면 응시 비율은 발표 중 정면을 응시하며 발표한 비율을 뜻 해. 발표의 정면 응시 비율이 60% 이상 90% 이하면 발표를 잘하고 있다는 것이고 60%보다 낮으면 신체는 정면을 향하지만 시선이 아래를 향하여 청중의 이목을 집중시키지 못하고 90%보다 높으면 너무 정면만 응시하여 청중의 집중을 이끌지 못해. 내 발표의 정면 응시 비율은 {np.round(gaze_percentage, 2)}%야. 이 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘."
+        content = f"발표의 정면 응시 비율은 (정면 응시 프레임의 합 / 전체 프레임의 합)%야. 발표의 정면 응시 비율은 발표 중 정면을 응시하며 발표한 비율을 뜻 해. 발표의 정면 응시 비율이 60% 이상 90% 이하면 발표를 잘하고 있다는 것이고 60%보다 낮으면 신체는 정면을 향하지만 시선이 아래를 향하여 청중의 이목을 집중시키지 못하고 90%보다 높으면 너무 정면만 응시하여 청중의 집중을 이끌지 못해. 내 발표의 정면 응시 비율은 {np.round(gaze_percentage, 2)}%야. 이 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘. \"정면 응시 분석 결과는 다음과 같습니다.\"라고 시작해."
         
     # GPT-3.5-TURBO API
     completion = client.chat.completions.create(
@@ -158,12 +159,12 @@ def gaze_analysis(gaze):
 # gap: gap_find_24의 find_silence()의 반환 값인 음성 공백 비율
 def gap_analysis(gap):
     # API 연동
-    OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = "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"
     client = OpenAI(api_key=OPENAI_API_KEY)
     
     # 음성 공백 평가 멘트
     gap_percentage = gap * 100
-    content = f"발표 음성의 공백 비율은 (음성 공백 시간(초) / 전체 발표 시간(초))%야. 발표 음성의 공백은 발표 중 아무 말도 안했다는 것을 뜻해. 발표 음성의 공백 비율이 6.7% 이하면 발표를 매끄럽게 잘하고 있다는 것이고 6.7% 초과면 발표 음성의 공백이 많아 발표가 매끄럽지 않다는 것이야. 내 발표 음성의 공백 비율은 {np.round(gap_percentage, 2)}%야. 이 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘."
+    content = f"발표 음성의 공백 비율은 (음성 공백 시간(초) / 전체 발표 시간(초))%야. 발표 음성의 공백은 발표 중 아무 말도 안했다는 것을 뜻해. 발표 음성의 공백 비율이 6.7% 이하면 발표를 매끄럽게 잘하고 있다는 것이고 6.7% 초과면 발표 음성의 공백이 많아 발표가 매끄럽지 않다는 것이야. 내 발표 음성의 공백 비율은 {np.round(gap_percentage, 2)}%야. 이 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘. \"음성 공백 분석 결과는 다음과 같습니다.\"라고 시작해."
 
     # GPT-3.5-TURBO API
     completion = client.chat.completions.create(
@@ -190,12 +191,12 @@ def gap_analysis(gap):
 # surplus: surplus_24의 find_surplus()의 반환 값인 음성적 잉여 비율
 def surplus_analysis(surplus):
     # API 연동
-    OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = "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"
     client = OpenAI(api_key=OPENAI_API_KEY)
     
     # 음성적 잉여 평가 멘트
     surplus_percentage = surplus * 100
-    content = f"불필요한 단어 언급 비율은 (불필요한 단어 개수 / 전체 단어 개수)%야. 불필요한 단어의 예시는 '아', '어', '음', '그' 야.불필요한 단어 언급 비율 값이 1% 이하면 발표를 잘하고 있다는 것이고 1% 초과면서 3% 이하면 발표를 적절히 하고 있다는 것이고 3%를 초과하면 불필요한 단어를 많이 말하고 있는거야. 내 발표에서 불필요한 단어 언급 비율은 {np.round(surplus_percentage, 2)}%야. 이 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘."
+    content = f"불필요한 단어 언급 비율은 (불필요한 단어 개수 / 전체 단어 개수)%야. 불필요한 단어의 예시는 '아', '어', '음', '그' 야.불필요한 단어 언급 비율 값이 1% 이하면 발표를 잘하고 있다는 것이고 1% 초과면서 3% 이하면 발표를 적절히 하고 있다는 것이고 3%를 초과하면 불필요한 단어를 많이 말하고 있는거야. 내 발표에서 불필요한 단어 언급 비율은 {np.round(surplus_percentage, 2)}%야. 이 비율에 대해서 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘. \"음성 잉여 분석 결과는 다음과 같습니다.\"라고 시작해."
 
     # GPT-3.5-TURBO API
     completion = client.chat.completions.create(
@@ -223,13 +224,13 @@ def surplus_analysis(surplus):
 # speed: speech_rate.py의 measure_speech_rate의 반환 값 (음성 속도 비율)
 def speed_analysis(speed):
     # API 연동
-    OPENAI_API_KEY = "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"
+    OPENAI_API_KEY = "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"
     client = OpenAI(api_key=OPENAI_API_KEY)
     
     # 음성 빠르기 평가 멘트
     speed_percentage = speed * 100
     print('speed_percentage:', speed_percentage)
-    content = f"발표 속도 평가 기준은 값이 35%보다 낮으면 너무 느린 거고 보다 80%보다 크면 매우 빠른 거야. 발표 속도가 35%보다 크면서 80%보다 작으면 적절한 발표 속도로 평가해야 돼. 내 발표 속도는 {np.round(speed_percentage, 2)}%이야. 이 발표 속도에 대해 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘."
+    content = f"발표 속도 평가 기준은 값이 35%보다 낮으면 너무 느린 거고 보다 80%보다 크면 매우 빠른 거야. 발표 속도가 35%보다 크면서 80%보다 작으면 적절한 발표 속도로 평가해야 돼. 내 발표 속도는 {np.round(speed_percentage, 2)}%이야. 이 발표 속도에 대해 발표와 관련하여 평가 리포트를 한 문단으로 만들어 줘. \"발표 속도 분석 결과는 다음과 같습니다.\"라고 시작해."
 
     # GPT-3.5-TURBO API
     completion = client.chat.completions.create(
