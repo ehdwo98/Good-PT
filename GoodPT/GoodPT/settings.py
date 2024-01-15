080233b5fe46f5062b72f3637a77cc32102492f3
Dong-Jae <wlsdk1095sd@gmail.com>
2024-01-15 11:19:00 +0900

    fix: settings.py

--- GoodPT/GoodPT/settings.py
+++ GoodPT/GoodPT/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hug5u5j)8%_npq+49fgx-&-9p!n78j-$c08x!np6rortc%u*mr"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
